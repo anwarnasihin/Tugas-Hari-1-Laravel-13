b3d1279d5ce2a53a77aef7b623f643928bd94d72
anwarnasihin <anwarnasihin555@gmail.com>
2026-06-10 14:49:10 +0700

    tugas laravel 13 pertemuan 1

---
+++ resources/views/about.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+</head>
+<body>
+    <h1>Tentang Saya</h1>
+
+    <img src="/Anwar.jpeg" alt="Foto Anwar Nasihin" width="150">
+
+    <br><br> <!-- Memberikan sedikit jarak antara foto dan teks -->
+
+    <table>
+        <tr>
+            <td><strong>Nama Lengkap</strong></td>
+            <td>:</td>
+            <td>Anwar Nasihin</td>
+        </tr>
+        <tr>
+            <td><strong>Usia</strong></td>
+            <td>:</td>
+            <td>32 tahun</td>
+        </tr>
+        <tr>
+            <td><strong>No HP</strong></td>
+            <td>:</td>
+            <td>+62823-1933-3774</td>
+        </tr>
+        <tr>
+            <td valign="top"><strong>Kesibukan Saat Ini</strong></td>
+            <td valign="top">:</td>
+            <td>Bekerja sebagai IT Support di Binus University, melakukan manajemen infrastruktur server (Linux, Proxmox), serta mendevelop aplikasi berbasis web dan mobile (Flutter).</td>
+        </tr>
+        <tr>
+            <td valign="top"><strong>Alasan Mengikuti Bootcamp Laravel 13 di Programmer Muda</strong></td>
+            <td valign="top">:</td>
+            <td>Ingin mendalami framework Laravel untuk optimasi backend, memperlancar manajemen database migration, dan mematangkan skill untuk persiapan transisi environment project ke Laravel 13.</td>
+        </tr>
+    </table>
+</body>
+</html>
